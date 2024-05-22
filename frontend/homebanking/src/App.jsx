05136a7a10abc444c1313fc785c5a5962f612e15
MariaABebion <nasty26@hotmail.com>
2024-05-22 20:51:01 -0300

    InactivityComponent
    Creacion del componente de inactividad.

--- frontend/homebanking/src/App.jsx
+++ frontend/homebanking/src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css'
 import Input from './components/Input/Input'
 import Button from './components/Button/Button'
+import InactivityComponent from './components/Inactivity/InactivityComponent'
+
 
 function App() {
 
   return (
     <>
+
       <h1>hello world</h1>
       
       {/* Voy cargando los componentes para ir viendo el render */}
       <Input placeholder="Jorge Martinez" img="/icons/person.svg"></Input>
       <Input placeholder="Clave Segura" img="/icons/Vector.svg"></Input>
       <Button title="Iniciar Sesión"></Button>
+          
+      <InactivityComponent />
+      
+    
     </>
   )
 }
