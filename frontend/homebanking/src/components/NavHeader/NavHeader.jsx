ca4f23175e47247ec03dda6ca0b9cd6442049436
kevin joel noviello <kjnoviello@gmail.com>
2024-06-23 21:19:07 -0300

    first

--- frontend/homebanking/src/components/NavHeader/NavHeader.jsx
+++ frontend/homebanking/src/components/NavHeader/NavHeader.jsx
@@ -9,11 +9,12 @@ const NavHeader = ({user, userPic, title, classHome}) => {
     const navHeaderClass = classHome ? "nav-heaver--home" : "nav-header";
     const titleNavHeader = user ? user : title; 
     const pics = userPic ?  <img src="/icons/arrow_left.png" alt="atrás" className=""/> : <img src="/icons/user.png" alt="atrás" className=""/>
+    const onClick = userPic ? "" : navigate(-1)
 
     return (
         <article className={navHeaderClass}>
             <div className="nav-header__left">
-                <button onClick={()=>{navigate(-1)} } className="nav-header__button ">
+                <button onClick={()=>{onClick} } className="nav-header__button ">
                     {pics}
                 </button>
                 <h3 className="nav-header__title">{titleNavHeader}</h3>
